601f537d4ac7b329df314f4845af2950619f437b
rbjanhefi <rizqi.janhefi@students.amikom.ac.id>
2020-04-15 14:59:18 +0700

    15042020

---
+++ Responsi Pemrograman 2899/Karyawan.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResponsiPemrograman2899
+{
+    class Karyawan
+    {
+        public string nik1;
+        public string nik2;
+        public string nama1;
+        public string nama2;
+        public float gaji1;
+        public float gaji2;
+
+        public void DataGaji()
+        {
+            Console.WriteLine("No Nik/Nama\t\t\tGaji Bulanan");
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("1. {0} {1}\t\t{2}", nik1, nama1, gaji1);
+            Console.WriteLine("2. {0} {1}\t\t{2}", nik2, nama2, gaji2);
+            Console.WriteLine(" ");
+        }
+
+        public void DataNaikGaji()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("Asyiiiiik kenaikan gaji 10%!!");
+            Console.WriteLine(" ");
+            Console.WriteLine("No Nik/Nama\t\t\tGaji Bulanan");
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("1. {0} {1}\t\t{2}", nik1, nama1, 0.1*gaji1+gaji1);
+            Console.WriteLine("2. {0} {1}\t\t{2}", nik2, nama2, 0.1*gaji2+gaji1);
+        }
+
+    }
+}
---
+++ Responsi Pemrograman 2899/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ResponsiPemrograman2899
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan = new Karyawan();
+
+            karyawan.nik1 = "190302123";
+            karyawan.nama1 = "Jono";
+            karyawan.gaji1 = 3000000;
+
+            karyawan.nik2 = "190302123";
+            karyawan.nama2 = "Paijo";
+            karyawan.gaji2 = 2000000;
+
+            karyawan.DataGaji();
+            karyawan.DataNaikGaji();
+        }
+    }
+}
